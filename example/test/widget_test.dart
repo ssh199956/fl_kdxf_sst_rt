04d6e0f20ddcad95310fcfb77f1a8d0ae9746370
shuaihong@6.cn <shuaihong@6.cn>
2023-07-12 13:48:56 +0800

    [Flutter]example修改

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -13,13 +13,13 @@ import 'package:fl_kdxf_sst_rt_example/main.dart';
 void main() {
   testWidgets('Verify Platform version', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyHomeXFPage());
 
     // Verify that platform version is retrieved.
     expect(
       find.byWidgetPredicate(
-        (Widget widget) => widget is Text &&
-                           widget.data!.startsWith('Running on:'),
+        (Widget widget) =>
+            widget is Text && widget.data!.startsWith('Running on:'),
       ),
       findsOneWidget,
     );
